fb355f781f115e2b64824f9ba6e4fea5114c149b
Thomas Young <Thomas.young228@gmail.com>
2022-01-07 21:19:31 -0500

    created test recycler view

--- app/src/main/java/com/thomasyoung/viewbindingrv/MainActivity.kt
+++ app/src/main/java/com/thomasyoung/viewbindingrv/MainActivity.kt
@@ -2,12 +2,26 @@ package com.thomasyoung.viewbindingrv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.thomasyoung.viewbindingrv.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        val adapter = MainAdapter(TaskList.taskList)
+        //binding?.taskRv?.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding?.taskRv?.adapter = adapter
+        }
 
+    override fun onDestroy(){
+        super.onDestroy()
+        binding = null
     }
 }
---
+++ app/src/main/java/com/thomasyoung/viewbindingrv/TaskList.kt
@@ -0,0 +1,15 @@
+package com.thomasyoung.viewbindingrv
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Take a Walk", timeStamp = "7:00 am"),
+        Task("Eat Breakfast", timeStamp = "8:00 am"),
+        Task("Attend a Meeting", timeStamp = "9:00 am"),
+        Task("Read a book", timeStamp = "11:00 am"),
+        Task("Eat Lunch", timeStamp = "12:00 pm"),
+        Task("Buy Groceries", timeStamp = "2:00 pm"),
+        Task("Watch videos", timeStamp = "3:00 pm"),
+        Task("Go to bed", timeStamp = "11:00 pm"),
+
+    )
+}
